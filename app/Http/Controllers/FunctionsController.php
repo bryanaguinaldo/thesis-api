158992eaf5c7450c80f32f8ce4ccbb68750bf507
Bryan Aguinaldo <bryanaguinaldo.e@gmail.com>
2023-02-14 21:09:30 +0800

    pp

---
+++ app/Http/Controllers/FunctionsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Entry;
+use Illuminate\Http\Request;
+
+class FunctionsController extends Controller
+{
+    public function verify(Entry $entry, Request $request)
+    {
+        $answer = $entry->pluck('answer')->first();
+
+        if ($request->input == null) return response()->json(null);
+
+        return strtolower($request->input) == strtolower($answer) ?
+            response()->json(['status' => 'OK', 'message' => 'correct answer']) :
+            response()->json(['status' => 'fail', 'message' => 'incorrect answer']);
+    }
+}
